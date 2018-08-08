c9e926d711bf298195f15bb462fe9919b080117f
Soroush <soroush.moazed@gmail.com>
2018-08-08 15:18:24 +0430

    encoding function replaced with numba implementation for speed performance. plot_spikes method changed to plot. dimension checking added, input signal and filter_responses must be 1d arrays now.

--- hsa.py
+++ hsa.py
@@ -2,6 +2,7 @@
 import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
+from numba import jit, float64, int8
 
 
 class HSAEncoder:
@@ -17,7 +18,7 @@ class HSAEncoder:
 
         Parameters
         ----------
-        filter_response : :obj: 'np.ndarray' , optional.
+        filter_response : :obj: 'np.ndarray' , 1d array. optional.
             Default: A gaussian signal with M=51, std=7.
             FIR filter as a window.
 
@@ -33,7 +34,7 @@ class HSAEncoder:
         self.filter_response = filter_response
         self.step = step
         self.filter_amp = filter_amp
-        self._last_spikes = None
+        self._last_spike_times = None
         self._last_signal = None
 
     @property
@@ -46,6 +47,7 @@ class HSAEncoder:
             self._filter_response = signal.gaussian(M=51, std=7)
         else:
             assert isinstance(new_response, np.ndarray), "'filter_response' must be of np.ndarray type."
+            assert new_response.ndim == 1, "'filter_response must be a 1d array."
             self._filter_response = new_response
 
     @property
@@ -73,44 +75,56 @@ class HSAEncoder:
 
         Parameters
         ----------
-        sgnl : :obj: 'np.ndarray'.
+        sgnl : :obj: 'np.ndarray' , 1d array.
             Signal to be encoded.
 
         Notes
         -----
-
+        The spike times will be save in self._last_spikes for later plottings.
+        The encoding procedure written in compiled mode using numba.jit decorator.
         """
 
+        @jit(int8[:](float64[:], float64[:], int8), nopython=True, cache=True)
+        def calc_spike_times(sgnl, filter_response, step):
+            filter_size = filter_response.shape[0]
+            sgnl_size = sgnl.shape[0]
+            windowed_signal = sgnl[:filter_size].copy()
+            spike_times = np.zeros(sgnl.shape, dtype=np.int8)
+            for pointer in range(0, sgnl_size, step):
+                if pointer > sgnl_size - filter_size - 1:
+                    break
+                else:
+                    if np.all(windowed_signal >= filter_response):
+                        windowed_signal -= filter_response
+                        spike_times[pointer] = 1
+                    windowed_signal = np.concatenate((windowed_signal[step:],
+                                                      sgnl[filter_size + pointer: filter_size + pointer + step]))
+            return spike_times
+
         assert isinstance(sgnl, np.ndarray), "'sgnl' must be of type numpy.ndarray"
-        filter_size = self.filter_response.shape[0]
+        assert sgnl.ndim == 1, "'sgnl' must be 1d array."
         filter_response = self.filter_response
         step = self.step
         self._last_signal = sgnl.copy()
-        sgnl_size = sgnl.shape[0]
-        windowed_signal = sgnl[:filter_size].copy()
-        spike_times = list()
-        for pointer in range(0, sgnl_size, step):
-            if pointer > sgnl_size - filter_size - 1:
-                break
-            else:
-                if np.all(windowed_signal >= filter_response):
-                    windowed_signal -= filter_response
-                    spike_times.append(pointer)
-                windowed_signal = np.array(windowed_signal[step:].tolist() +
-                                           sgnl[filter_size + pointer: filter_size + pointer + step].tolist())
-        self._last_spikes = spike_times
-        spikes = np.zeros(sgnl.shape)
-        spikes[spike_times] = 1
+        spikes = calc_spike_times(sgnl=sgnl, filter_response=filter_response, step=step)
+        self._last_spike_times = np.where(spikes == 1)
         return spikes
 
-    def plot_spikes(self):
+    def plot(self):
         """Plot encoded version and original version of last signal."""
 
         assert self._last_signal is not None, "You must encode at least one signal to perform plotting."
         fig, [ax0, ax1] = plt.subplots(2, 1)
         fig.figsize = (18, 12)
         ax0.plot(self._last_signal)
-        ax1.eventplot(self._last_spikes)
+        ax1.eventplot(self._last_spike_times)
         ax1.set_xlim(-0.1 * len(self._last_signal), 1.1 * len(self._last_signal))
         ax1.set_yticks([1])
         plt.show()
+
+
+sgnl = np.sin(np.linspace(0, 10, 1000))
+f_response = 0.1 * signal.gaussian(M=51, std=7)
+encoder = HSAEncoder(filter_amp=0.2)
+encoder.encode(sgnl=sgnl)
+encoder.plot()
