355b808d8d020a9c46c562d1eba70e76ac0c6d38
Soroush <soroush.moazed@gmail.com>
2018-08-12 14:12:50 +0430

    added decode attribute in examples.

--- eamples/bsa_example.py
+++ eamples/bsa_example.py
@@ -4,6 +4,8 @@ import bsa
 s = np.sin(np.linspace(0, 10, 1000))
 noise = np.random.rand(1000)/5
 s += noise
+s = (1 + s)/2
 encoder = bsa.BSAEncoder()
 encoder.encode(sgnl=s)
 encoder.plot()
+encoder.decode()
--- eamples/hsa_example.py
+++ eamples/hsa_example.py
@@ -1,9 +1,11 @@
 import numpy as np
-import bsa
+import hsa
 
 s = np.sin(np.linspace(0, 10, 1000))
 noise = np.random.rand(1000)/5
 s += noise
-encoder = bsa.BSAEncoder()
+s = (1 + s)/2
+encoder = hsa.HSAEncoder()
 encoder.encode(sgnl=s)
 encoder.plot()
+encoder.decode()
