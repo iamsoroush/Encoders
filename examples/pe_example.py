039926637b0d5115dfbf3944ee3bc93d78888341
Soroush <soroush.moazed@gmail.com>
2018-08-25 22:48:18 +0430

    added a better sample signal for encoding.

--- examples/pe_example.py
+++ examples/pe_example.py
@@ -1,9 +1,8 @@
-from phase_encoders import PhaseEncoderUnit
+from phase_encoders import TemporalPhaseEncoder
 import numpy as np
 
 
-s = np.sin(2 * np.pi * 50 * np.linspace(0, 1, 1000))
-noise = np.random.rand(1000)/5
-s += noise
-encoder = PhaseEncoderUnit()
-encoder.encode(sgnl=s)
+sample_points = np.linspace(0, 1, 1000)
+sig = np.cos(2 * np.pi * 50 * sample_points) + np.cos(2 * np.pi * 70 * sample_points) + np.random.random(1000)/2
+encoder = TemporalPhaseEncoder()
+encoder.encode(sgnl=sig)
